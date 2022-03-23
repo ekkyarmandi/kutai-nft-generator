35bd3fcf51fcb6ef01ef3b801bf7bca070ea4b6e
Ekky Armandi <ekkyarmandi@gmail.com>
2022-03-23 09:52:03 +0700

    add CLI feature

--- manage.py
+++ manage.py
@@ -1,19 +1,64 @@
 import os
+from random import choices
 import sys
+
+import argparse
 
 from scripts import util
 from engine import layers_merger, generator
 
-if sys.argv[1] == "generate":
+parser = argparse.ArgumentParser(description='Artwork Random Generative Engine')
+
+parser.add_argument(
+    "command",
+    type=str,
+    choices=["merge","generate"],
+    help="Main command line"
+)
+
+parser.add_argument(
+    "-n", "--number",
+    type=int,
+    nargs=1,
+    default=10,
+    help="Number of generated output"
+)
+
+parser.add_argument(
+    "-s", "--size-type",
+    type=str,
+    choices=["default","custom"],
+    default="default",
+    help="Size custom width and height"
+)
+
+parser.add_argument(
+    "-mp", "--merge-path",
+    type=str,
+    default="layers",
+    help="Layers path folder"
+)
+
+parser.add_argument(
+    "-p", "--path",
+    type=str,
+    default="project\\example",
+    help="Project path folder"
+)
+
+args = parser.parse_args()
+
+# if args.command == "merge" and "layers" in args.path:
+#     raise argparse.ArgumentError(path, "Path for Merge command should be Layers path")
+
+if args.command == "generate":
         
-    project_folder = os.path.join("project",sys.argv[3])
+    project_folder = os.path.join("project",args.path)
     util.check_folder(project_folder)
-    
-    metadata_folder = os.path.join(project_folder,"metadata")
-    util.check_folder(metadata_folder)
 
-    generator(number=sys.argv[2], output_path=project_folder)
+    generator(number=args.number, output_path=project_folder)
 
-elif sys.argv[1] == "compile":
+elif args.command == "merge":
     
-    layers_merger(source_path="layers", size_type="default")
+    layers_merger(source_path=args.merge_path, size_type=args.size_path)
+
