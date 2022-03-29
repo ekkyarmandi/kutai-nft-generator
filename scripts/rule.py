c131298f4ae1780f8e38a988631c1b7be74cf8e4
Ekky Armandi <ekkyarmandi@gmail.com>
2022-03-29 20:42:38 +0800

    adding more parameter to the choose function

--- scripts/rule.py
+++ scripts/rule.py
@@ -1,9 +1,8 @@
 import random
 import json
     
-def choose(attributes, key):
-    probability = json.load(open("source\\probability.json"))
-    weights = [probability[key][attr] for attr in attributes[key]]
+def choose(attributes, probabilities, trait_type):
+    weights = [probabilities[trait_type][attr] for attr in attributes[trait_type]]
     weights = [p/sum(weights) for p in weights]
-    item = random.choices(attributes[key], weights=weights, k=1)
+    item = random.choices(attributes[trait_type], weights=weights, k=1)
     return item[0]
