afe261e9a5b4c62894ed0799144e8954620d384c
Ekky Armandi <ekkyarmandi@gmail.com>
2022-03-29 17:54:29 +0700

    updating the requirements.txt

--- test.py
+++ test.py
@@ -1,15 +1,1 @@
-import os
-import sys
 
-from scripts import util
-from engine import layers_merger, generator
-
-layers_merger(source_path="layers", size_type="default")
-
-project_folder = os.path.join("project","example")
-util.check_folder(project_folder)
-
-metadata_folder = os.path.join(project_folder,"metadata")
-util.check_folder(metadata_folder)
-
-generator(output_path=project_folder)
