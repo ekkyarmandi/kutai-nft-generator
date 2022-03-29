fcfc4fe6ee420916bc4fe336170763ccca9e19bd
Ekky Armandi <ekkyarmandi@gmail.com>
2022-03-30 01:30:46 +0800

    modifying command line

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ import json
 import os
 
 import argparse
+from this import d
 
 from scripts import utils
 from engine import KutaiEngine
@@ -10,57 +11,94 @@ engine = KutaiEngine()
 
 parser = argparse.ArgumentParser(description='Artwork Random Generative Engine')
 
-commands = [
+subparser = parser.add_subparsers(title="Main command lines")
+
+starting = subparser.add_parser(
     "startproject",
+    help="Create your project directory"
+)
+starting.add_argument(
+    "startproject",
+    action="store_true",
+    default=False
+)
+starting.add_argument(
+    "project_path",
+    help="Your project path destination"
+)
+
+read = subparser.add_parser(
     "read_layers",
-    "update_attributes",
-    "generate",
-    "export"
-]
-
-parser.add_argument(
-    "command",
-    type=str,
-    choices=commands,
-    help="Main command line"
+    help="Read layers project directory"
 )
-
-parser.add_argument(
-    "-n", "--number",
-    type=int,
-    default=10,
-    help="Number of generated output"
+read.add_argument(
+    "read_layers",
+    action="store_true",
+    default=False
 )
-
-parser.add_argument(
-    "-s", "--size-type",
-    type=str,
-    choices=["default","custom"],
-    default="default",
-    help="Size custom width and height"
-)
-
-parser.add_argument(
+read.add_argument(
     "-p", "--path",
     type=str,
-    default="project\\example",
-    help="Project path folder"
+    default="layers",
+    help="Project path"
 )
 
-parser.add_argument(
-    "-op", "--output-path",
-    type=str,
-    default="project\\example\\output\\sample",
-    help="Output path folder inside your project path"
+generate = subparser.add_parser(
+    "generate",
+    help="Generate project metadata"
 )
+generate.add_argument(
+    "generate",
+    action="store_true",
+    default=False
+)
+generate.add_argument(
+    "number",
+    metavar="NUMBER",
+    type=int,
+    help="Desired output"
+)
+generate.add_argument(
+    "-o","--output",
+    default="output\\example",
+    help="Metadata output destination path"
+)
+
+export = subparser.add_parser(
+    "export",
+    help="Export images based on metadata"
+)
+export.add_argument(
+    "export",
+    action="store_true",
+    default=False
+)
+export.add_argument(
+    "-o","--output",
+    default="output\\example",
+    help="Metadata output destination path"
+)
+
+# parser.add_argument(
+#     "-s", "--size-type",
+#     type=str,
+#     choices=["default","custom"],
+#     default="default",
+#     help="Size custom width and height"
+# )
 
 args = parser.parse_args()
 
-if args.command == "startproject":
+print(args)
+
+if args.startproject:
     
     # create project folder
-    project_path = os.path.join("project",args.path)
-    utils.check_folder(project_path)
+    if args.project_path == ".":
+        project_path = ""
+    else:
+        project_path = args.project_path
+        utils.check_folder(project_path)
 
     # create sub folders
     sub_folders = [
@@ -90,20 +128,17 @@ if args.command == "startproject":
     # print out the message
     print(project_path,"has been created")
 
-elif args.command == "read_layers":
+elif args.read_layers:
     engine.read_layers(args.path)
 
-elif args.command == "update_attributes":
-    engine.update_attributes(args.path)
-
-elif args.command == "generate":
+elif args.generate:
     engine.generate_metadata(
         number=args.number,
         project_path=args.path,
         output_path=args.output_path
     )
 
-elif args.command == "export":
+elif args.export:
     engine.export(
         project_path=args.path,
         output_path=args.output_path
