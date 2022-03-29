58772c0a3bc8fd27d2c067e1811fd696b74a7e82
Ekky Armandi <ekkyarmandi@gmail.com>
2022-03-29 17:08:14 +0800

    Remove GTK Runtime from the project

--- GTK3-Runtime Win64/gtk3-runtime/gtk3r-env.bat
+++
@@ -1,9 +0,0 @@
-@set GTK3R_PREFIX=C:\Program Files\GTK3-Runtime Win64\bin
-
-@echo Setting environment variables for GTK3-Runtime.
-@echo.
-
-@echo set PATH=%GTK3R_PREFIX%;%%PATH%%
-@set PATH=%GTK3R_PREFIX%;%PATH%
-
-@echo.
